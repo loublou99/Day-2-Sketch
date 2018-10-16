f4ac74b0b2308dbfd6bc5371c3e8e7d68c52a3ef
Laura Jones <lauramjones@live.co.uk>
2018-10-16 11:46:08 +0100

    Added moving is true

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var moving = true
 var name = 'Laura Jones'
 //Background size - canvas
 // var position - initial position on the canvas
@@ -10,8 +11,13 @@ function draw() {
   //Add space before speech mark - adds space when it displays it (so number isn't right next to it)
   console.log('The position is' + position)
   background(100)
+  text('Moving is ' + moving, 10, 70)
 //Position + 1 - causes it to move by one each time from original position
 position = position + 1
 rect(position, 0, 10, 10)
 text('My name is ' + name, 10, 30)
+}
+
+function mousePressed() {
+  moving = !moving
 }
