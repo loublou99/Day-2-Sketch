9f0d6bdd04b689394b316e0fb9ec2a2c8d59e56f
Laura Jones <lauramjones@live.co.uk>
2018-10-17 11:23:19 +0100

    Added feature where the square moves from side to side

--- sketch.js
+++ sketch.js
@@ -3,8 +3,16 @@ var name = 'Laura Jones'
 //Background size - canvas
 // var position - initial position on the canvas
 var position = 100
-function setup () {
+var moving = true
+var speed = 1
+
+
+function setup() {
   createCanvas(400, 200)
+}
+
+function mousePressed() {
+  moving = !moving
 }
 //Background (colour) and shape (including size/dimensions)
 function draw() {
@@ -12,14 +20,18 @@ function draw() {
   console.log('The position is' + position)
   background(100)
   text('Moving is ' + moving, 10, 70)
-//Position + 1 - causes it to move by one each time from original position
-if (moving) {
-position = position + 1
-}
-rect(position, 0, 10, 10)
-text('My name is ' + name, 10, 30)
+  //Position + 1 - causes it to move by one each time from original position
+  position = position + speed
+  if (position >= 400) {
+   speed = -speed
+  }
+    if (position == 0) {
+   speed = -speed
+  }
+  rect(position, 0, 10, 10)
+  text('My name is ' + name, 10, 30)
 }
 
-function mousePressed() {
-  moving = !moving
-}
+
+
+
